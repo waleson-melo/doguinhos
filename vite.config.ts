c604b21cf53f103ef6fc6d47c4a6b3bf079cd61b
Waleson Melo <walesonmelo23@gmail.com>
2025-11-12 12:31:40 -0400

    Add custom plugin to generate 404.html from index.html during build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,31 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), // Plugin customizado para gerar o 404.html
+    {
+      name: "copy-404",
+      closeBundle() {
+        const distPath = path.resolve(__dirname, "dist");
+        const indexPath = path.join(distPath, "index.html");
+        const notFoundPath = path.join(distPath, "404.html");
+
+        if (fs.existsSync(indexPath)) {
+          fs.copyFileSync(indexPath, notFoundPath);
+          console.log("✅ 404.html gerado automaticamente.");
+        } else {
+          console.warn(
+            "⚠️ index.html não encontrado. Rode 'vite build' antes."
+          );
+        }
+      },
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -14,5 +35,5 @@ export default defineConfig({
   server: {
     allowedHosts: [".ngrok-free.app"],
   },
-  base: '/doguinhos/'
+  base: "/doguinhos/",
 });
